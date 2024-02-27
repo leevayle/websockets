e11938b0bf8d00b3f148ca6d0e0e329a5b1b536b
lee kinanga <lee254kinanga@gmail.com>
2024-02-28 00:26:56 +0300

    first working commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 7758});
+
+wss.on("connection", (ws)=>{
+    console.log(`New user connected`);
+
+    ws.on("message", data => {
+        console.log(`Client said: ${data}`);
+
+        ws.send(`Backend is looking good baby!`);
+    });
+
+    ws.on("close", ()=>{
+        console.log("User disconnected");
+    });
+});
+
